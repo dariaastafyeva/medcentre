2fcb33befb5d8631c6a50ed3b0768803b19637da
Daria Astafyeva <mermalya17@gmail.com>
2022-04-21 16:58:31 +0300

    A css file was added

--- medcentre/settings.py
+++ medcentre/settings.py
@@ -132,4 +132,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'medcentre/static'),]
